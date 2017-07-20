b962412afbe7c151688cb7a26efa5a6f564022c5
Kanokwan Sudleam <kanokwan.sud@kasikornbank.com>
2017-07-20 11:26:36 +0700

    fb case30

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,6 +1,6 @@
 public class FizzBuzz {
     public String say(int input) {
-        if(input == 15){
+        if(input%15 == 0){
             return "FizzBuzz";
         }
         if(input%3 == 0){
